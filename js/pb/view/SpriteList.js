2f7529dad049775dc197d282255de2136846995f
Michael Buettner <michael.buettner@seitenbau.com>
2013-07-19 16:11:29 +0200

    Sprite list supports sprite selection

--- js/pb/view/SpriteList.js
+++ js/pb/view/SpriteList.js
@@ -31,12 +31,20 @@
                             var sprites = this.spriteSheet.sprites;
                             var $sprites = $('.pb-spritelist .pb-sprites');
                             for (var i = 0; i < sprites.length; i++) {
-                                $sprites.append(sprites[i]);
+                                var cvs = sprites[i];
+                                $(cvs).data('index', i);
+                                $sprites.append(cvs);
+
+                                if (i === 0) {
+                                    $(cvs).addClass('selected');
+                                }
                             }
                         }
                     }, this);
 
-                    this.observe(this.messages, 'sheet:new', this.onSheetChanged, this);
+                    this.observe(this.messages, 'sheet:changed', this.onSheetChanged, this);
+
+                    this.observeDom('#sidebar', 'canvas', 'click', this.handleSpriteClick);
                 };
             }),
 
@@ -45,13 +53,21 @@
                 this.dirty = true;
             },
 
-            getData: alchemy.override(function (_super) {
-                return function () {
-                    var data = _super.call(this);
-                    data.spriteSheet = this.spriteSheet;
-                    return data;
-                };
-            }),
+            handleSpriteClick: function (e) {
+                var $sprite = e && $(e.target);
+                var data = $sprite && $sprite.data();
+                var index = data && data.index;
+
+                if (index >= 0) {
+                    $('#sidebar .pb-sprites canvas.selected').removeClass('selected');
+                    $sprite.addClass('selected');
+
+                    this.messages.trigger('sprite:selected', {
+                        sheet: this.spriteSheet,
+                        index: index
+                    });
+                }
+            }
         }
     });
 }());
