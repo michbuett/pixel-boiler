39dfe4c4970070c4bfebbe97d4276748bd0ad1ac
michbuett <michbuett@gmx.de>
2013-11-12 12:31:18 +0100

    Introduce more generic override mechanism

--- js/nw/overrides.js
+++ js/nw/overrides.js
@@ -3,6 +3,62 @@
 
     var alchemy = require('./Alchemy.js');
 
+    var overrides = [{
+        name: 'pb.Sheeter',
+        extend: 'pb.Sheeter',
+        overrides: {
+            /** @lends nw.Sheeter.prototype */
+
+            load: function (cfg) {
+                console.log('[nw.Sheeter] load', cfg);
+            },
+
+            save: function (cfg) {
+                console.log('[nw.Sheeter] save', cfg);
+                if (!cfg || !cfg.canvas) {
+                    return;
+                }
+
+                if (cfg.filePath) {
+                    this.saveSpriteSheet(cfg.filePath, cfg.canvas);
+
+                    if (alchemy.isFunction(cfg.success)) {
+                        cfg.success.call(cfg.scope);
+                    }
+                } else {
+                    this.selectSaveAsFile(function (file) {
+                        this.save(alchemy.mix(cfg, {
+                            filePath: file.path
+                        }));
+                    }, this);
+                }
+            },
+
+            saveSpriteSheet: function (filePath, canvas) {
+                var imgData = canvas.toDataURL().replace(/^data:image\/\w+;base64,/, '');
+                var buffer = new Buffer(imgData, 'base64');
+                window.fileSystem.writeFileSync(filePath, buffer);
+            },
+
+            selectSaveAsFile: function (cb, scope) {
+                $('body').append('<input id="saveas" type="file" nwsaveas="Untitled.png">');
+                var $saveAs = $('input#saveas');
+
+                $saveAs.on('change', function () {
+                    var file = $saveAs[0].files[0];
+                    $saveAs.remove();
+                    cb.call(scope, file);
+                });
+
+                $saveAs.on('abort', function () {
+                    $saveAs.remove();
+                });
+
+                $saveAs.click();
+            },
+        }
+    }];
+
     /**
      * Description
      *
@@ -12,42 +68,15 @@
      */
     alchemy.formula.add({
         name: 'nw.overrides',
-        extend: 'alchemy.core.MateriaPrima',
         overrides: {
             /** @lends nw.Overrides.prototype */
 
-            requires: ['pb.Sheeter'],
-
+            /**
+             * Applies the node-webkit specific overrides
+             */
             apply: function () {
-                alchemy.extend(alchemy('pb.Sheeter'), {
-                    load: this.openSpriteSheet,
-                    save: this.saveSpriteSheet,
-                });
+                alchemy.each(overrides, alchemy.brew);
             },
-
-            openSpriteSheet: function (cfg) {
-                console.log('[nw.Overrides] openSpriteSheet', cfg);
-            },
-
-            saveSpriteSheet: function (cfg) {
-                $('body').append('<input id="saveas" type="file" nwsaveas="' + cfg.filename + '">');
-                var $saveAs = $('input#saveas');
-
-                $saveAs.on('change', function () {
-                    var file = $saveAs[0].files[0];
-                    var imgData = cfg.canvas.toDataURL().replace(/^data:image\/\w+;base64,/, '');
-                    var buffer = new Buffer(imgData, 'base64');
-                    window.fileSystem.writeFileSync(file.path, buffer);
-                    $saveAs.remove();
-                });
-
-                $saveAs.on('abort', function () {
-                    $saveAs.remove();
-                });
-
-                $saveAs.click();
-            },
-
         }
     });
 }());
