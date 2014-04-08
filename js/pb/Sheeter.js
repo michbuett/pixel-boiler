a0fda85d64096faa9688c420c6d6f86c07f92994
Michael Buettner <michbuett@gmx.de>
2014-04-08 09:57:08 +0200

    Fix the fix
    While fixing initial sprite size based on the file name the import got broken

--- js/pb/Sheeter.js
+++ js/pb/Sheeter.js
@@ -236,6 +236,10 @@
                 this.observe(view, 'click .cancel', view.close, view);
                 this.observe(view, 'close', this.closeActiveDialog, this);
 
+                this.observe(view, 'rendered', function () {
+                    var img = $('#selected-image')[0].src = dataUrl;
+                }, this);
+
                 this.observe(view, 'click .confirm', function () {
                     var img = $('#selected-image')[0];
                     var src = img.src;
